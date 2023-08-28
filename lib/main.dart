9f09f10f159dda6c4b0c36fc19326b50005311da
abihanafi23 <hanafi.abi50@gmail.com>
2023-08-28 20:55:41 +0700

    FIX ORIENTATION BUG

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,27 @@ import 'utils.dart';
 import 'firstpage.dart';
 import 'package:flutter/services.dart';
 
-@override
-void initState(){
-  SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
   ]);
+  runApp(const MyApp());
 }
-
-void main() => runApp(const MyApp());
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-	return MaterialApp(
-		title: 'Chemsfun',
-		debugShowCheckedModeBanner: false,
-		scrollBehavior: MyCustomScrollBehavior(),
-		theme: ThemeData(
-		primarySwatch: Colors.cyan,
-		),
-		home: const FirstPage(),
-	);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chemsfun',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const FirstPage(),
+    );
+  }
 }
